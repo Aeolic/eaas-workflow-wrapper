2a010d78c5c7c9e5e789a0f96f9afae4e7ea0870
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-26 19:55:08 +0000

    Adding tarfile member sanitization to extractall()

--- v2/wrapper_v2.py
+++ v2/wrapper_v2.py
@@ -157,7 +157,26 @@ def main():
         f.write(blobstore_response.content)
 
     with tarfile.open(temp_dir + "/files.tgz", "r:gz") as zip_ref:
-        zip_ref.extractall(path=temp_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(zip_ref, path=temp_dir)
 
     os.remove(temp_dir + "/files.tgz")
 
